06b6f07af5ac10ed680935b8fc40f44da75107db
aniketcodes <aniketsinha1304@gmail.com>
2022-01-04 12:56:53 +0530

    added validation in email

--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
 const mongoose = require( "mongoose" );
+const validator = require( "validator" );
 
 const userSchema = new mongoose.Schema( {
   name: {
-    type: String
+    type: String,
+    required:true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate( value ) {
+      if ( !validator.isEmail( value ) ) {
+        throw new Error( "Invalid email" );
+      }
+    }
   },
   age: {
     type: Number,
