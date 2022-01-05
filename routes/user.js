a5e3abcb5321b7e57da1c901df3a4b21f2f950e6
aniketcodes <aniketsinha1304@gmail.com>
2022-01-05 18:49:22 +0530

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const userController = require( "../controllers/userController" );
 const auth = require( "../middleware/auth" );
 router.post( '/create', userController.createUser );
-router.get( "/me",auth, userController.viewMe );
-router.get( "/:id", userController.viewUser );
+router.get( "/me", auth, userController.viewMe );
+router.patch( "/me", userController.updateUser );
+router.delete( "/me", auth, userController.deleteUser );
 router.post( '/login', userController.login );
+router.get( "/logout", auth, userController.logout );
 
 module.exports = router
